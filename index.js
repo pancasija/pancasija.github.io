d6ec72ecae5160d933d73fdb78dbf893e52fa70e
TheGenocides <luke.genesis.hyder@gmail.com>
2023-09-21 21:37:45 +0700

    refactor: Complete first page

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const header = document.getElementById("welcome")
 const navbar = document.getElementById("navbar")
 let range = 3
 
+function isElementInViewport(element) {
+	const rect = element.getBoundingClientRect();
+	return (
+	  rect.top >= 0 &&
+	  rect.left >= 0 &&
+	  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+	  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+  }
+
 function triggerNavbar(x) {
 	if (x.classList.contains("change")){
 		for (let child of navbar.children){
@@ -29,7 +39,23 @@ function triggerNavbar(x) {
 	x.classList.toggle("change");
 }
 
+// Function to handle the scroll event
+
+	
+  
+// Attach the handleScroll function to the window's scroll event
+window.addEventListener('scroll', () => {
+	if (isElementInViewport(welcome)) {
+		header.style.opacity = 1;
+		header.style.transform = "translateY(-15px)"
+	} else {
+		header.style.opacity = 0;
+		header.style.transform = "translateY(15px)"
+	}
+	
+});
+
 setTimeout(_ => {
-	header.style.opacity = 1
-	header.style.transform = "translateY(-15px)"
+	header.style.opacity = 1;
+	header.style.transform = "translateY(-15px)";
 }, 300)
